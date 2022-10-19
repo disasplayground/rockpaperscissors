652630c627c339ef7f1c3a738302e88120d1fc26
disasplayground <disa.tannos@outlook.com>
2022-10-19 12:18:43 +0800

    Add rounds & scoring

--- app.js
+++ app.js
@@ -1,57 +1,72 @@
 let user = "";
 let play = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let userScore = 0;
 
 
-function start() {
+function round() {
 
-    user = (prompt("Type Rock/Paper/Scissors!")).toLowerCase();
-    return user;
+    user = (prompt("Computer: " + computerScore + " | You: " + userScore + "\nType Rock/Paper/Scissors!")).toLowerCase();
+
+    let number = Math.floor(Math.random()*3);
+    let computer = play[number];
+
+    let win = "Computer plays " + computer + ". You win!";
+    let lose = "Computer plays " + computer + ". You lose!";
+    let draw = "Computer plays " + computer + ". It's a draw!";
+
+    if (user === computer) {
+        alert(draw)
+    } else {
+        if (user === "rock") {
+            if (computer === "paper") {
+                computerScore++;
+                alert(lose);
+            } else {
+                userScore++;
+                alert(win);
+            } 
+        } if (user === "paper") {
+            if (computer === "scissors") {
+                computerScore++;
+                alert(lose);
+            } else {
+                userScore++;
+                alert(win);
+            } 
+        } if (user === "scissors") {
+            if (computer === "rock") {
+                computerScore++;
+                alert(lose);
+            } else {
+                userScore++;
+                alert(win);
+            } 
+        } if (user !== "rock" && user !== "paper" && user !== "scissors") {
+            alert("What is that.")
+        };
+    };
 
 };
 
 
 function game() {
 
-    let number = Math.ceil(Math.random()*3);
-    let computer = play[number];
+    for (let i = 0; i < 5; i++) {
+        
+        round();
 
-    let win = "Computer plays " + computer + ". You win!";
-    let lose = "Computer plays " + computer + ". You lose!";
-    let draw = "Computer plays " + computer + ". It's a draw!"
-
-    if (user === computer) {
-        alert(draw)
-    } else {
-        if (user === "rock") {
-            switch(computer) {
-                case "paper":
-                    alert(lose);
-                    break;
-                case "scissors":
-                    alert(win);
-                    break;
+        if (i === 4) {
+            if (computerScore > userScore) {
+                alert("Computer: " + computerScore + " | You: " + userScore + "\nYou lose.")
+            } if (computerScore < userScore) {
+                alert("Computer: " + computerScore + " | You: " + userScore + "\nYou lose.")
+            } if (computerScore === userScore) {
+                alert("It's a draw!")
             }
-        } if (user === "paper") {
-            switch(computer) {
-                case "scissors":
-                    alert(lose);
-                    break;
-                case "rock":
-                    alert(win);
-                    break;
-            }
-        } if (user === "scissors") {
-            switch(computer) {
-                case "rock":
-                    alert(lose);
-                    break;
-                case "paper":
-                    alert(win);
-                    break;
-            }
-        } else {
-            alert("What is that.")
-        }
+        };
     };
 
 };
+
+game();
